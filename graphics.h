e4a1c3eb84f9043f03b752b9923a2921353d7c80
Joshua-Newton <Joshua.Newton@uvm.edu>
2019-03-13 13:51:35 -0400

    first commit-barebones graphics

---
+++ graphics.h
@@ -0,0 +1,51 @@
+//
+// Created by Joshua Newton on 2019-03-13.
+//
+
+#ifndef CS205FINAL_GRAPHICS_H
+#define CS205FINAL_GRAPHICS_H
+
+#include <stdlib.h>
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <sys/time.h>
+#endif
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+// Program initialization NOT OpenGL/GLUT dependent,
+// as we haven't created a GLUT window yet
+void init(void);
+
+// Initialize OpenGL Graphics
+void InitGL(void);
+
+// Callback functions for GLUT */
+
+// Draw the window - this is where all the GL actions are
+void display(void);
+
+// Trap and process alphanumeric keyboard events
+void kbd(unsigned char key, int x, int y);
+
+// Trap and process special keyboard events
+void kbdS(int key, int x, int y);
+
+// Handle "mouse cursor moved" events
+void cursor(int x, int y);
+
+// Calls itself after a specified time
+void timer(int extra);
+
+// Handle mouse button pressed and released events
+void mouse(int button, int state, int x, int y);
+
+int graphicsPlay(int argc, char *argv[]);
+
+
+#endif //CS205FINAL_GRAPHICS_H
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "graphics.h"
+
+int main(int argc, char** argv){
+    std::cout << "Hello, World!" << std::endl;
+    graphicsPlay(argc, argv);
+    return 0;
+}
