21ebed08829a7721d81deb3555df78e4f469fd70
Joshua Newton <Joshua.Newton@uvm.edu>
2019-03-15 20:19:37 -0400

    Basic Structure of Cards

---
+++ Card.cpp
@@ -0,0 +1,102 @@
+//
+// Created by jonew on 3/15/2019.
+//
+
+#include "Card.h"
+/***********************************/
+/** ABSTRACT CARD CLASS BEGINNING **/
+/***********************************/
+
+// Constructors
+Card::Card(){
+    setDescription("Default Constructor Used");
+    setActivation(1,12);
+    setCost(1);
+}
+Card::Card(string newDescription, int newCost, range newActivation){
+    setDescription(newDescription);
+    setCost(newCost);
+    setActivation(newActivation);
+}
+Card::Card(string newDescription, int newCost, int newActivationMin, int newActivationMax){
+    setDescription(newDescription);
+    setCost(newCost);
+    setActivation(newActivationMin, newActivationMax);
+}
+// Destructor
+Card::~Card(){
+
+}
+
+// Getters
+string Card::getDescription() const{
+    return description;
+}
+int Card::getCost() const{
+    return cost;
+}
+range Card::getActivation() const{
+    return activation;
+}
+int Card::getActivationMin() const{
+    return activation.minimum;
+}
+int Card::getActivationMax() const{
+    return activation.maximum;
+}
+RectangleShape Card::getRectangle() const {
+    return rectangle;
+}
+
+// Setters
+void Card::setDescription(string newDescription){
+    description = newDescription;
+}
+void Card::setCost(int newCost){
+    // Ensure that the cost is not negative before accepting it
+    if(newCost >= 0){
+        cost = newCost;
+    }
+    // Set to 0 if it is negative
+    else{
+        cost = 0;
+    }
+}
+void Card::setActivation(range newRange){
+    //Note: input validation occurs inside of the two functions called
+    setActivationMin(newRange.minimum);
+    setActivationMax(newRange.maximum);
+}
+void Card::setActivation(int newMin, int newMax){
+    //Note: input validation occurs inside of the two functions called
+    setActivationMax(newMax);
+    setActivationMin(newMin);
+}
+void Card::setActivationMin(int newMin){
+    if (newMin >= 1 && newMin <= 12){
+        activation.minimum = newMin;
+    }
+    else {
+        activation.minimum = 1;
+    }
+}
+void Card::setActivationMax(int newMax){
+    if (newMax >= 1 && newMax <= 12){
+        activation.minimum = newMax;
+    }
+    else {
+        activation.minimum = 12;
+    }
+}
+void Card::setRectangle(RectangleShape newRectangle) {
+    rectangle = newRectangle;
+}
+
+// Virtual Methods
+/* Overridden in child classes */
+
+// Other Methods
+
+/***********************************/
+/** ABSTRACT CARD CLASS ENDING    **/
+/***********************************/
---
+++ Card.h
@@ -0,0 +1,73 @@
+//
+// Created by jonew on 3/15/2019.
+//
+
+#ifndef CS205FINAL_CARD_H
+#define CS205FINAL_CARD_H
+
+#include <string>
+#include "Shapes.h"
+
+using namespace std;
+
+struct range {
+    int minimum;
+    int maximum;
+};
+
+/**
+ * primaryIndustry = Blue
+ * secondaryIndustry = Green
+ * restaurants = Red
+ * majorEstablishment = Purple
+ * landmark = Yellow (Gray if not constructed)
+ */
+enum cardType {primaryIndustry, secondaryIndustry, restaurants, majorEstablishment, landmark};
+
+class Card{
+public:
+    // Constructors
+    Card();
+    Card(string newDescription, int newCost, range newActivation);
+    Card(string newDescription, int newCost, int newActivationMin, int newActivationMax);
+    // Destructor
+    ~Card();
+
+    // Getters
+    string getDescription() const;
+    int getCost() const;
+    range getActivation() const;
+    int getActivationMin() const;
+    int getActivationMax() const;
+    RectangleShape getRectangle() const;
+
+    // Setters
+    void setDescription(string newDescription);
+    void setCost(int newCost);
+    void setActivation(range newRange);
+    void setActivation(int newMin, int newMax);
+    void setActivationMin(int newMin);
+    void setActivationMax(int newMax);
+    void setRectangle(RectangleShape newRectangle);
+
+    // Virtual Methods
+    virtual void draw() const = 0;
+    virtual void activate() const = 0;
+
+    // Other Methods
+
+protected:
+    string description;
+    int cost;
+    range activation;
+    RectangleShape rectangle;
+};
+
+// TODO: Machi Koro cards will inherit the above abstract Card class.
+//  Cards Remaining:
+//  Wheat Field, Ranch, Forest, Mine, Apple Orchard, Bakery, Convenience Store, Cheese Factory, Furniture Factory,
+//  Fruit and Vegetable Market, Cafe, Family Restaurant, Stadium, TV Station, Business Center, Train Station,
+//  Shopping Mall, Amusement Park, Radio Tower
+
+
+#endif //CS205FINAL_CARD_H
