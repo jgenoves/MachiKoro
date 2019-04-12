79aec14861a277edae3ec386bf1f0f1d697c4826
Ethan Nerney <enerney@uvm.edu>
2019-04-12 16:24:14 -0400

    Redid file reading loop

--- Image.cpp
+++ Image.cpp
@@ -30,59 +30,6 @@ void Image::draw() const {
 double Image::calculateArea() const {return 0.;}
 double Image::calculatePerimeter() const {return 0.;}
 
-void readPNG(string filename, vector<vector<Pixel>> &pixels) {
-
-    std::ifstream file;
-    size_t size = 0;
-
-    std::cout << "Attempting to open " << filename << std::endl;
-
-    file.open( filename, std::ios::in | std::ios::binary | std::ios::ate );
-
-    unsigned int width, height;
-
-    file.seekg(16);
-    file.read((char *)&width, 4);
-    file.read((char *)&height, 4);
-
-    width = ntohl(width);
-    height = ntohl(height);
-
-    std::cout << "Picture is " << width << " pixels wide and " << height << " pixels high.\n";
-
-    file.seekg( 0, std::ios::end );
-    size = file.tellg();
-    std::cout << "File size: " << size << std::endl;
-    file.seekg( 0, std::ios::beg );
-
-    char data[size - 8];
-    file.seekg( 8 ); // skip the header
-    file.read( data, size - 8);
-    std::cout << "Data size: " << ((unsigned long long)file.tellg() - 8) << std::endl;
-
-    if (file)
-        std::cout << "all characters read successfully." << endl;
-    else
-        std::cout << "error: only " << file.gcount() << " could be read" << endl;
-
-    vector<RGBColor> colors;
-    for (int i = 0; i < size-8; i+=3){
-       cout << int(data[i]) << endl;
-    }
-
-
-    int j = 0;
-    for (int h = 0; h < height; h++) {
-        vector<Pixel> p;
-        for (int w = width-1; w >= 0; w--) {
-            p.push_back(Pixel(Vector2D(w,h), colors[j]));
-            j++;
-        }
-        j += 1;
-        pixels.push_back(p);
-    }
-
-}
 
 void readBMP(string filename, vector<vector<Pixel>> &pixels) {
     filename = "../" + filename;
@@ -114,30 +61,38 @@ void readBMP(string filename, vector<vector<Pixel>> &pixels) {
 
     char temp = 0;
 
-    vector<RGBColor> colors;
+    vector <RGBColor> colors;
+
     for (int i = dataSize - 4; i >= 0; i -= 3)
     {
-        //cout << i << endl;
         temp = img[i];
         img[i] = img[i+2];
         img[i+2] = temp;
 
-        int r = int(img[i] & 0xff), g = int(img[i+1] & 0xff), b = int(img[i+2] & 0xff);
+        int r,g,b;
+        r = img[i] & 0xff;
+        g = img[i] & 0xff;
+        b = img[i+2] & 0xff;
 
+        colors.push_back(RGBColor(g,r,b));
 
-        colors.push_back(RGBColor(b,r,g));
-        std::cout << "R: " << int(img[i] & 0xff) << " G: " << int(img[i+1] & 0xff) << " B: " << int(img[i+2] & 0xff) << std::endl;
+        //std::cout << "R: " << int(img[i] & 0xff) << " G: " << int(img[i+1] & 0xff) << " B: " << int(img[i+2] & 0xff) << std::endl;
+
     }
-
+    std::cout << "Length of Colors: " << colors.size() << std::endl;
+    std::cout << "Number of Pixels: " << width * height << std::endl;
     int j = 0;
-    for (int h = 0; h < height; h++) {
-        vector<Pixel> p;
-        for (int w = width-1; w >= 0; w--) {
-            p.push_back(Pixel(Vector2D(w,h), colors[j]));
+    for (int c = 0; c < height; c++) {
+        vector<Pixel> col;
+        for (int r = 0; r < width; r++) {
+            Pixel p(Vector2D(r,c), colors[j]);
+            col.push_back(p);
             j++;
         }
-        j += 1;
-        pixels.push_back(p);
+
+        pixels.push_back(col);
     }
+
+
 
 }
